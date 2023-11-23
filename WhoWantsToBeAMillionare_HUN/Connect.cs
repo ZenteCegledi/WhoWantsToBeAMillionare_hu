77b90e68c2c96d7d0dd1f2a9deaf1b39a62591f9
Ceglédi Zente Holló <czh@czh.hu>
2023-11-23 11:16:19 +0100

    MySQL Connect file

---
+++ WhoWantsToBeAMillionare_HUN/Connect.cs
@@ -0,0 +1,25 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhoWantsToBeAMillionare_HUN
+{
+    static class Connect
+    {
+        internal static MySqlConnection conn;
+        public static void initDB()
+        {
+            // Csatlakozás
+            MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
+            builder.Server = "localhost";
+            builder.UserID = "root";
+            builder.Password = "mypass";
+            builder.Database = "loim";
+
+            conn = new MySqlConnection(builder.ToString());
+        }
+    }
+}
